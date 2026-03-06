aed20484f6a4f5a243eb54f3ad0324f677868fc9
sumra800 <sumramalik84@gmail.com>
2026-03-06 06:13:03 +0500

    same

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = (process.env.REACT_APP_API_URL || 'http://localhost:5000').replace(/\/$/, '');
 const API_BASE_URL = `${API_URL}/api`;
 
 async function apiRequest(endpoint, options = {}) {
